b2be666f25d2ed96d20164da97ef47c9e08fa92c
Held Grijo <von.grijo@pmm.am.gov.br>
2019-06-17 11:19:20 -0400

    Hello world through server

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-//executar no terminal: node index.js
-console.log("hello node");
+const http = require("http");
+
+http
+  .createServer((req, res) => {
+    console.log(req); //imprime a requisição
+    return res.end("Hello Node"); //responde a requisição
+  })
+  .listen(3000);
+
+//acessar http://localhost:3000 no navegador para testar
