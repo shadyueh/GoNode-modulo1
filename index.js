7d335b687a03c59a31e055966e508a0aad05ed55
Held Grijo <von.grijo@pmm.am.gov.br>
2019-07-03 11:28:34 -0400

    Prettified on save (vscode settings: "eslint.autoFixOnSave": true,)

--- index.js
+++ index.js
@@ -1,38 +1,38 @@
-const express = require("express");
-const nunjucks = require("nunjucks");
+const express = require('express')
+const nunjucks = require('nunjucks')
 
-const app = express();
+const app = express()
 
 // template engine nunjucks
-nunjucks.configure("views", {
+nunjucks.configure('views', {
   autoescape: true,
   express: app,
   watch: true
-});
+})
 
 // express manuseia as informações vinda no form
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false }))
 
 // configura a extensão para o arquivo de templates
-app.set("view engine", "njk");
+app.set('view engine', 'njk')
 
-const users = ["Held Grijo", "Gabriel Souza", "Gabriel Dantas"];
+const users = ['Held Grijo', 'Gabriel Souza', 'Gabriel Dantas']
 
 // renderizando uma view com nunjucks, passando parâmetros
-app.get("/", (req, res) => {
-  return res.render("list", { users });
-});
+app.get('/', (req, res) => {
+  return res.render('list', { users })
+})
 
-app.get("/new", (req, res) => {
-  return res.render("new");
-});
+app.get('/new', (req, res) => {
+  return res.render('new')
+})
 
-app.post("/create", (req, res) => {
-  users.push(req.body.user);
-  return res.redirect("/");
-});
+app.post('/create', (req, res) => {
+  users.push(req.body.user)
+  return res.redirect('/')
+})
 
-app.listen(3000);
+app.listen(3000)
 
 // use yarn start para iniciar o server com o nodemon
 // acessar http://localhost:3000 no navegador para testar
