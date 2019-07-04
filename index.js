169584350ebed4ed9f94e29f95d3dd22f5a68d89
Held Grijo <von.grijo@pmm.am.gov.br>
2019-07-04 11:15:35 -0400

    Route for "/check". Redirection handling.

--- index.js
+++ index.js
@@ -10,13 +10,29 @@ nunjucks.configure("views", {
   watch: true
 });
 
+// express manuseia as informações vinda no form
+app.use(express.urlencoded({ extended: false }));
+
 // define a extensão para o arquivo de templates
 app.set("view engine", "njk");
 
-//responde a requisição para '/'
+// responde a requisição para '/'
 app.get("/", (req, res) => {
   // renderizando uma view com nunjucks, passando parâmetros
   return res.render("form");
+});
+
+// responde a requisição para '/'
+app.post("/check", (req, res) => {
+  // captura parâmetro vindo no corpo da requisição '/'
+  let age = parseInt(req.body.age, 10);
+  // tratamento solicitado no desafio
+  let path = "minor";
+  if (age > 18) {
+    path = "major";
+  }
+  // redirecionamento conforme tratamento
+  return res.redirect(`/${path}?age=` + age);
 });
 
 app.listen(3000);
