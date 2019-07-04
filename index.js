e6975f5dbbb748db903f85a5005ffa21561b8325
Held Grijo <von.grijo@pmm.am.gov.br>
2019-07-04 10:30:45 -0400

    Addding Express, Nunjucks, view and route for root request ("/")

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-const http = require("http");
+const express = require("express");
+const nunjucks = require("nunjucks");
 
-http
-  .createServer((req, res) => {
-    console.log(req); //imprime a requisição
-    return res.end("Hello Node"); //responde a requisição
-  })
-  .listen(3000);
+const app = express();
+
+// template engine nunjucks
+nunjucks.configure("views", {
+  autoescape: true,
+  express: app,
+  watch: true
+});
+
+// define a extensão para o arquivo de templates
+app.set("view engine", "njk");
+
+//responde a requisição para '/'
+app.get("/", (req, res) => {
+  // renderizando uma view com nunjucks, passando parâmetros
+  return res.render("form");
+});
+
+app.listen(3000);
 
 // iniciar a app com yarn start (scripts em package.json já configurado)
 // acessar http://localhost:3000 no navegador para testar
